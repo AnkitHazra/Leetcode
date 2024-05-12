cfb57be09e19e832dbed0ac89cec39efd0279bbe
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-05-12 22:12:45 +0530

    Time: 3 ms (23.36%), Space: 7.1 MB (75.23%) - LeetHub

---
+++ 3371-harshad-number/3371-harshad-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int n=x;
+        int sum=0;
+        while(n!=0){
+                sum=sum+(n%10);
+                n=n/10;
+        }
+        if(x%sum==0){
+            return sum;
+        }
+        else{
+            return -1;
+        }
+    }
+};
