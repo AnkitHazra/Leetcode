9f3f43f8949e468e2babbb1ea2a121cddc02fed2
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-05-14 16:25:02 +0530

    Time: 15 ms (53.6%), Space: 19.3 MB (31.95%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int count;
-        for(int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            count = 0; // Reset count for each new number
-            for(int j = 0; j < nums.size(); j++) {
-                if(num == nums[j]) {
-                    count++;
-                }
-            }
-            if(count == 1) {
-                return num;
-            }
+        int xorr=0;
+        for(int i=0;i<nums.size();i++){
+            xorr=xorr^nums[i];
         }
-        // If no unique number is found, return a default value or handle the case appropriately
-        return -1; // For example, returning -1 to indicate no unique number found
-    }  
+        return xorr;
+    }
 };
