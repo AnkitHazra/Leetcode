700b8557cbd3853bf4f510a356213f4b2fa8c4f6
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-05-24 12:20:43 +0530

    Time: 0 ms (100%), Space: 7.2 MB (50.02%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int tribonacci(int n) {
-       if(n==0) {
-            return 0;
+        if(n<2) return n;
+        int ans[]={0,1,1};
+        for(int i=3;i<=n;i++){
+            int next=ans[0]+ans[1]+ans[2];
+            ans[0]=ans[1];
+            ans[1]=ans[2];
+            ans[2]=next;
         }
-        if(n==2 || n==1){ return 1;
-        }
-        else{
-        long long int firstterm=0;
-        long long int secondterm=1;
-        long long int thirdterm=1;
-        
-        for(int i=0;i<n;i++){
-           long long int fourthterm=firstterm+secondterm+thirdterm;
-            firstterm=secondterm;
-            secondterm=thirdterm;
-            thirdterm=fourthterm;
-        }
-        
-     return   firstterm;
-        }
+        return ans[2];
     }
 };
