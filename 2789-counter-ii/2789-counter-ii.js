57b93fb0511fc8524127617ceebfdf6379d43faf
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-06-22 09:46:00 +0530

    Time: 59 ms (58.52%), Space: 50.9 MB (96.94%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let c=init;
+    function increment(){
+        return ++c;
+    }
+    function decrement(){
+        return --c;
+    }
+    function reset(){
+        c=init;
+        return c;
+    }
+    return {increment,decrement,reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
