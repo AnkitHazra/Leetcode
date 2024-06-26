a0859cce64b60aef037f9d993fd1be76429d2d76
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-06-26 20:09:06 +0530

    Time: 38 ms (9.06%), Space: 94.6 MB (57.54%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+    int n = n1 + n2; //total size
+    //required indices:
+    int ind2 = n / 2;
+    int ind1 = ind2 - 1;
+    int cnt = 0;
+    int ind1el = -1, ind2el = -1;
+
+    //apply the merge step:
+    int i = 0, j = 0;
+    while (i < n1 && j < n2) {
+        if (nums1[i] < nums2[j]) {
+            if (cnt == ind1) ind1el = nums1[i]; 
+            if (cnt == ind2) ind2el = nums1[i]; 
+            cnt++;
+            i++;
+        }
+        else {
+            if (cnt == ind1) ind1el = nums2[j]; 
+            if (cnt == ind2) ind2el = nums2[j]; 
+            cnt++;
+            j++;
+        }
+    }
+
+    //copy the left-out elements:
+    while (i < n1) {
+        if (cnt == ind1) ind1el = nums1[i]; 
+        if (cnt == ind2) ind2el = nums1[i];
+        cnt++;
+        i++;
+    }
+    while (j < n2) {
+        if (cnt == ind1) ind1el = nums2[j]; 
+        if (cnt == ind2) ind2el = nums2[j];
+        cnt++;
+        j++;
+    }
+
+    //Find the median:
+    if (n % 2 == 1) {
+        return (double)ind2el;
+    }
+
+    return (double)((double)(ind1el + ind2el)) / 2.0;
+    }
+};
