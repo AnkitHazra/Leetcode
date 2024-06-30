dd6ca97ba9479fef8e8a3d8815c6e228ef918f52
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-06-30 09:22:12 +0530

    Time: 4 ms (41.2%), Space: 9.1 MB (71.84%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,35 +1,37 @@
 class Solution {
-int func(vector<int>&arr,int maxsum){
-    int n=arr.size();
-    int partions=1;
-    long long subarray=0;
-    for(int i=0;i<n;i++){
-    if(subarray+arr[i]<=maxsum){
-        subarray+=arr[i];
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
     }
-    else{
-        partions++;
-        subarray=arr[i];
-    }
-    }
-    return partions;
+    return partitions;
 }
+
 public:
     int splitArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(k>n) return -1;
-        int low=*max_element(nums.begin(),nums.end());
-        int high=accumulate(nums.begin(),nums.end(),0);
-        while(low<=high){
-            int mid=(low+high)/2;
-            int maxsub=func(nums,mid);
-            if(maxsub>k){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+        int low = *max_element(nums.begin(), nums.end());
+    int high = accumulate(nums.begin(), nums.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int partitions = countPartitions(nums, mid);
+        if (partitions > k) {
+            low = mid + 1;
         }
-        return low;
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
     }
 };
