d5d6514d5fc55295afd83f007d4e92744e2135dc
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-07-08 18:26:02 +0530

    Time: 3 ms (23.59%), Space: 7.3 MB (36.85%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        if(n <= 0) return 0;
-        double x = log2(n);
-        return x == (int)x;   
+        if (n > 0 && (n & (n - 1)) == 0) return true;
+        else return false;  
     }
 };
