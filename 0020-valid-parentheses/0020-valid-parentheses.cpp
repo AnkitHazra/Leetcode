8b57bc332b53099db8005edc569d982aab6c53f9
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-07-17 14:07:09 +0530

    Time: 3 ms (41.29%), Space: 7.8 MB (26.11%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char ch=st.top();
+                st.pop();
+                if((s[i]==']' && ch=='[')||(s[i]=='}' && ch=='{')||(s[i]==')' && ch=='(')){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+
+        }
+        return st.empty();
+    }
+};
