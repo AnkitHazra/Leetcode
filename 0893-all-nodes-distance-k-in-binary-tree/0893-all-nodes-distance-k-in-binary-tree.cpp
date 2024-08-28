6d45ec8249d588af710ec5ca32632ebb142fce77
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2024-08-29 00:43:17 +0530

    Time: 0 ms (100%), Space: 15.6 MB (35.11%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent_track) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* current = q.front();
+            q.pop();
+            if (current->left) {
+                parent_track[current->left] = current;
+                q.push(current->left);
+            }
+            if (current->right) {
+                parent_track[current->right] = current;
+                q.push(current->right);
+            }
+        }
+    }
+public:
+     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*, TreeNode*> parent_track;
+        markParents(root, parent_track);
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*> q;
+        q.push(target);
+        visited[target] = true;
+        int curr_level = 0;
+        while (!q.empty()) {
+            int n = q.size();
+            if (curr_level == k) break;
+            curr_level++;
+            for (int i = 0; i < n; i++) {
+                TreeNode* current = q.front();
+                q.pop();
+                if (current->left && !visited[current->left]) {
+                    q.push(current->left);
+                    visited[current->left] = true;
+                }
+                if (current->right && !visited[current->right]) {
+                    q.push(current->right);
+                    visited[current->right] = true;
+                }
+                if (parent_track[current] && !visited[parent_track[current]]) {
+                    q.push(parent_track[current]);
+                    visited[parent_track[current]] = true;
+                }
+            }
+        }
+        vector<int> res;
+        while (!q.empty()) {
+            TreeNode* current = q.front();
+            res.push_back(current->val);
+            q.pop();
+        }
+        return res;
+    }
+};
