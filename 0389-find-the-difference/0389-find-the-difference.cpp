6612bd2d10852752d48a2a69984aff1970c043ca
Ankit Hazra <118801726+AnkitHazra@users.noreply.github.com>
2025-05-24 20:56:06 +0530

    Time: 0 ms (100%), Space: 9.1 MB (95.37%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans =0;
+        for(char c :s){
+            ans^=c;
+        }
+        for(char c:t){
+            ans^=c;
+        }
+        return ans;
+    }
+};
